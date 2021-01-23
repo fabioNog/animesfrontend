daa7b97f6552a2f88cc38fdcff9c9d3c9639cfdb
fabioNog <fabio.carmo@computacao.ufla.br>
2021-01-23 17:10:48 -0300

    Alter package.json and add pages with index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+const rc = require("rc");
+
+import React, { Component } from 'react'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                ola mundo
+            </div>
+        )
+    }
+}
